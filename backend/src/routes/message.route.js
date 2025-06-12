b657d6280b76a470b7a8d96e3e5afbdd747f6f17
Rahul P <rahul.cs23@bitsathy.ac.in>
2025-06-12 18:57:02 +0530

    project completed

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,5 +4,5 @@ import { getMessages, getUsersForSideBar, sendMessage } from "../controllers/mes
 const router = express.Router();
 router.get('/users',protectRoute,getUsersForSideBar);
 router.get('/:id',protectRoute,getMessages);
-router.post('/send/:id',protectRoute,sendMessage)
+router.post('/send/:id',protectRoute,sendMessage);
 export default router;
